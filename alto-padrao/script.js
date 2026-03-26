2081927ebedaa65b52cc93cb47dd2a545ed2b21e
Rodrigo Angelo Evangelista <hiukky.ra@gmail.com>
2026-03-26 18:50:14 -0300

    ajustes finos

---
+++ alto-padrao/script.js
@@ -0,0 +1,43 @@
+// Navigation Scrolled State
+const navbar = document.getElementById('navbar');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+// Scroll behavior simples / Intersection Observer
+const scrollElements = document.querySelectorAll('.scroll-anim');
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return (elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend);
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add('visible');
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.15)) {
+      displayScrollElement(el);
+    }
+  })
+}
+
+window.addEventListener('scroll', () => { handleScrollAnimation(); });
+setTimeout(() => { handleScrollAnimation(); }, 200);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    if (this.getAttribute('href') === '#') return;
+    e.preventDefault();
+    const dest = document.querySelector(this.getAttribute('href'));
+    if (dest) {
+      dest.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
